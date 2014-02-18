49541b051586c7b6ff4a0069a7c34ae885e16646
thomasbottonari <thomas.bottonari@gmail.com>
2014-02-17 22:34:57 -0500

    cleaned up and added colors

--- AnimatedButton.js
+++ AnimatedButton.js
@@ -1,15 +1,15 @@
 function AnimatedButton (R, label, width, height) {
-	var txtAttr = {font: '20px Oswald', fill: "#000"}
-	var rect = R.rect(0,0,width,height,20).attr({fill: '#0f0', stroke: 'none', opacity:0})
+	var txtAttr = {font: '20px Oswald', fill: "#7d7d7b"}
+	var rect = R.rect(0,0,width,height,20).attr({fill: '#fff', stroke: 'none', opacity:0})
 	var txt = R.text(width/2, height/2, label).attr(txtAttr);
-	var touchArea = R.rect(0,0,width,height).attr({fill: '#000', stroke: 'none', opacity:0})
+	var touchArea = R.rect(0,0,width,height).attr({fill: '#fff', stroke: 'none', opacity:0})
 	rect.transform("s0.1");
 	touchArea.mouseover(function() {
 		rect.animate({opacity:1, transform:"s1"},200);
-		txt.animate({fill: '#fff'},200);
+		txt.animate({fill: '#36EBFF'},200);
 	});
 	touchArea.mouseout(function() {
 		rect.animate({opacity:0, transform:"s0.1"},200);
-		txt.animate({fill: '#000'},200);
+		txt.animate({fill: '#7d7d7b'},200);
 	});
 }
