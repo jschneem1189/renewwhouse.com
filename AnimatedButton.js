c70f9eb904ae6534fbfd83e530dc4365b8799a00
thomasbottonari <thomas.bottonari@gmail.com>
2014-02-23 18:17:29 -0500

    full screen demo

--- AnimatedButton.js
+++ AnimatedButton.js
@@ -1,5 +1,5 @@
 function AnimatedButton (R, label, width, height) {
-	var txtAttr = {font: '20px Oswald', fill: "#7d7d7b"}
+	var txtAttr = {font: '20px Oswald', fill: "#fff"}
 	var rect = R.rect(0,0,width,height,20).attr({fill: '#fff', stroke: 'none', opacity:0})
 	var txt = R.text(width/2, height/2, label).attr(txtAttr);
 	var touchArea = R.rect(0,0,width,height).attr({fill: '#fff', stroke: 'none', opacity:0})
@@ -10,6 +10,6 @@ function AnimatedButton (R, label, width, height) {
 	});
 	touchArea.mouseout(function() {
 		rect.animate({opacity:0, transform:"s0.1"},200);
-		txt.animate({fill: '#7d7d7b'},200);
+		txt.animate({fill: '#fff'},200);
 	});
 }
