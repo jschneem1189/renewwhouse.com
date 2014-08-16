74935b8a68f7268ada5720f7fadbcd4194bc4de5
thomasbottonari <thomas.bottonari@gmail.com>
2014-08-16 09:45:30 -0400

    added retrieval of monthly data

--- data/getEnergyData.php
+++ data/getEnergyData.php
@@ -78,56 +78,61 @@ if ($year === 0) {
 	  	$this_year++;
 	}
 } else if ($month === 0) {
-	$sql="SELECT SUM(`CH1-Inactive`) + 
-			SUM(`CH2-Main Power`) + 
-			SUM(`CH3-Main Power`) + 
-			SUM(`CH4-Oven`) + 
-			SUM(`CH5-Oven`) + 
-			SUM(`CH6-Furnace closet light`) + 
-			SUM(`CH7-Office outlet`) + 
-			SUM(`CH8-Basement outlets and lights`) + 
-			SUM(`CH9-Office outlet{1}`) + 
-			SUM(`CH10-Dining Room outlets`) + 
-			SUM(`CH11-Basement closet light`) + 
-			SUM(`CH12-Basement lights`) + 
-			SUM(`CH13-Living Room outlet`) + 
-			SUM(`CH14-Air Conditioner`) + 
-			SUM(`CH15-Air Conditioner`) + 
-			SUM(`CH16-Basement outlet`) + 
-			SUM(`CH17-Basement lights{1}`) + 
-			SUM(`CH18-First Floor lights and outlets`) + 
-			SUM(`CH19-Kitchen outlet`) + 
-			SUM(`CH20-clothes dryer`) + 
-			SUM(`CH21-clothes dryer`) + 
-			SUM(`CH22-Second Floor lights and outlets`) + 
-			SUM(`CH23-S bedroom outlet`) + 
-			SUM(`CH24-First floor outlets and lights`) + 
-			SUM(`CH25-East bedroom outlets`) + 
-			SUM(`CH26-Second Floor outlets`) + 
-			SUM(`CH27-Refrigerator`) + 
-			SUM(`CH28-Inactive`) + 
-			SUM(`CH29-Inactive`) + 
-			SUM(`CH30-Inactive`) + 
-			SUM(`CH31-Inactive`) + 
-			SUM(`CH32-Inactive`) + 
-			SUM(`CH33-Inactive`) + 
-			SUM(`CH34-Inactive`) + 
-			SUM(`CH35-Inactive`) + 
-			SUM(`CH36-Inactive`) + 
-			SUM(`CH37-Inactive`) + 
-			SUM(`CH38-Inactive`) + 
-			SUM(`CH39-Inactive`) + 
-			SUM(`CH40-Inactive`) + 
-			SUM(`CH41-Inactive`) + 
-			SUM(`CH42-Inactive`) + 
-			SUM(`CH43-Inactive`) + 
-			SUM(`CH44-Inactive`) + 
-			SUM(`CH45-Inactive`) AS 'Jan' FROM eMonitor 
-			WHERE `Date/Time` >= '".$year."-01-01'
-			AND `Date/Time` <= '".$year."-01-31'";
+	// SHOW ALL MONTHS
+	$labels = array('Jan','','Mar','','May','','July','','Sep','','Nov','');
+	for ($i=0; $i<=12; $i++) {
+		$sql="SELECT SUM(`CH1-Inactive`) + 
+				SUM(`CH2-Main Power`) + 
+				SUM(`CH3-Main Power`) + 
+				SUM(`CH4-Oven`) + 
+				SUM(`CH5-Oven`) + 
+				SUM(`CH6-Furnace closet light`) + 
+				SUM(`CH7-Office outlet`) + 
+				SUM(`CH8-Basement outlets and lights`) + 
+				SUM(`CH9-Office outlet{1}`) + 
+				SUM(`CH10-Dining Room outlets`) + 
+				SUM(`CH11-Basement closet light`) + 
+				SUM(`CH12-Basement lights`) + 
+				SUM(`CH13-Living Room outlet`) + 
+				SUM(`CH14-Air Conditioner`) + 
+				SUM(`CH15-Air Conditioner`) + 
+				SUM(`CH16-Basement outlet`) + 
+				SUM(`CH17-Basement lights{1}`) + 
+				SUM(`CH18-First Floor lights and outlets`) + 
+				SUM(`CH19-Kitchen outlet`) + 
+				SUM(`CH20-clothes dryer`) + 
+				SUM(`CH21-clothes dryer`) + 
+				SUM(`CH22-Second Floor lights and outlets`) + 
+				SUM(`CH23-S bedroom outlet`) + 
+				SUM(`CH24-First floor outlets and lights`) + 
+				SUM(`CH25-East bedroom outlets`) + 
+				SUM(`CH26-Second Floor outlets`) + 
+				SUM(`CH27-Refrigerator`) + 
+				SUM(`CH28-Inactive`) + 
+				SUM(`CH29-Inactive`) + 
+				SUM(`CH30-Inactive`) + 
+				SUM(`CH31-Inactive`) + 
+				SUM(`CH32-Inactive`) + 
+				SUM(`CH33-Inactive`) + 
+				SUM(`CH34-Inactive`) + 
+				SUM(`CH35-Inactive`) + 
+				SUM(`CH36-Inactive`) + 
+				SUM(`CH37-Inactive`) + 
+				SUM(`CH38-Inactive`) + 
+				SUM(`CH39-Inactive`) + 
+				SUM(`CH40-Inactive`) + 
+				SUM(`CH41-Inactive`) + 
+				SUM(`CH42-Inactive`) + 
+				SUM(`CH43-Inactive`) + 
+				SUM(`CH44-Inactive`) + 
+				SUM(`CH45-Inactive`) AS 'monthly' FROM eMonitor 
+				WHERE `Date/Time` >= '".$year."-".$i."-01'
+				AND `Date/Time` <= '".$year."-".$i."-31'";
 
 		$result = mysqli_query($con,$sql);
 		$row = $result->fetch_row();
+		array_push($values, $row[0]);
+	}
 }
 echo "{\"labels\":";
 echo json_encode($labels);
