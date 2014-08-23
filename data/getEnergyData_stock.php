aa00b370020e21f297dba29a6e0c4223b03f5923
thomasbottonari <thomas.bottonari@gmail.com>
2014-08-23 01:11:34 -0400

    changed query to reflect kWh

--- data/getEnergyData_stock.php
+++ data/getEnergyData_stock.php
@@ -1,6 +1,7 @@
 <?php
+// header('Access-Control-Allow-Origin: *');
 // ini_set('memory_limit', '-1');
-$con = mysqli_connect('localhost','reneww','renewwhouse','house_data');
+$con = mysqli_connect('23.229.157.253','reneww','renewwhouse','house_data');
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -14,96 +15,11 @@ if (!$link) {
 $labels = array();
 $values = array();
 
-$sql="SELECT date(`Date/Time`) as date, SUM(`CH1-Inactive` + 
-	`CH2-Main Power` + 
-	`CH3-Main Power` + 
-	`CH4-Oven` + 
-	`CH5-Oven` + 
-	`CH6-Furnace closet light` + 
-	`CH7-Office outlet` + 
-	`CH8-Basement outlets and lights` + 
-	`CH9-Office outlet{1}` + 
-	`CH10-Dining Room outlets` + 
-	`CH11-Basement closet light` + 
-	`CH12-Basement lights` + 
-	`CH13-Living Room outlet` + 
-	`CH14-Air Conditioner` + 
-	`CH15-Air Conditioner` + 
-	`CH16-Basement outlet` + 
-	`CH17-Basement lights{1}` + 
-	`CH18-First Floor lights and outlets` + 
-	`CH19-Kitchen outlet` + 
-	`CH20-clothes dryer` + 
-	`CH21-clothes dryer` + 
-	`CH22-Second Floor lights and outlets` + 
-	`CH23-S bedroom outlet` + 
-	`CH24-First floor outlets and lights` + 
-	`CH25-East bedroom outlets` + 
-	`CH26-Second Floor outlets` + 
-	`CH27-Refrigerator` + 
-	`CH28-Inactive` + 
-	`CH29-Inactive` + 
-	`CH30-Inactive` + 
-	`CH31-Inactive` + 
-	`CH32-Inactive` + 
-	`CH33-Inactive` + 
-	`CH34-Inactive` + 
-	`CH35-Inactive` + 
-	`CH36-Inactive` + 
-	`CH37-Inactive` + 
-	`CH38-Inactive` + 
-	`CH39-Inactive` + 
-	`CH40-Inactive` + 
-	`CH41-Inactive` + 
-	`CH42-Inactive` + 
-	`CH43-Inactive` + 
-	`CH44-Inactive` + 
-	`CH45-Inactive`) AS val, 
-	SUM(`CH1-Inactive` + 
-	`CH2-Main Power` + 
-	`CH3-Main Power` + 
-	`CH4-Oven` + 
-	`CH5-Oven` + 
-	`CH6-Furnace closet light` + 
-	`CH7-Office outlet` + 
-	`CH8-Basement outlets and lights` + 
-	`CH9-Office outlet{1}` + 
-	`CH10-Dining Room outlets` + 
-	`CH11-Basement closet light` + 
-	`CH12-Basement lights` + 
-	`CH13-Living Room outlet` + 
-	`CH14-Air Conditioner` + 
-	`CH15-Air Conditioner` + 
-	`CH16-Basement outlet` + 
-	`CH17-Basement lights{1}` + 
-	`CH18-First Floor lights and outlets` + 
-	`CH19-Kitchen outlet` + 
-	`CH20-clothes dryer` + 
-	`CH21-clothes dryer` + 
-	`CH22-Second Floor lights and outlets` + 
-	`CH23-S bedroom outlet` + 
-	`CH24-First floor outlets and lights` + 
-	`CH25-East bedroom outlets` + 
-	`CH26-Second Floor outlets` + 
-	`CH27-Refrigerator` + 
-	`CH28-Inactive` + 
-	`CH29-Inactive` + 
-	`CH30-Inactive` + 
-	`CH31-Inactive` + 
-	`CH32-Inactive` + 
-	`CH33-Inactive` + 
-	`CH34-Inactive` + 
-	`CH35-Inactive` + 
-	`CH36-Inactive` + 
-	`CH37-Inactive` + 
-	`CH38-Inactive` + 
-	`CH39-Inactive` + 
-	`CH40-Inactive` + 
-	`CH41-Inactive` + 
-	`CH42-Inactive` + 
-	`CH43-Inactive` + 
-	`CH44-Inactive` + 
-	`CH45-Inactive`) * 0.5 AS val2 FROM eMonitor GROUP BY date(`Date/Time`)";
+$sql="SELECT date(`Date/Time`) as date, 
+	SUM((`CH2-Main Power` + 
+		`CH3-Main Power`) / 60 / 1000) AS val, 
+	SUM((`CH2-Main Power` + 
+		`CH3-Main Power`) * 0.5 / 60 / 1000) AS val2 FROM eMonitor GROUP BY date(`Date/Time`)";
 
 $result = mysqli_query($con,$sql);
 // $row = $result->fetch_row();
