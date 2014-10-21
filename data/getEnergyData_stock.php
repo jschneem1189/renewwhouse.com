109acf6624b7f516e16b95f42cdd3dda09db6cc3
thomasbottonari <thomas.bottonari@gmail.com>
2014-10-20 22:15:08 -0400

    changed to localhost

--- data/getEnergyData_stock.php
+++ data/getEnergyData_stock.php
@@ -2,8 +2,8 @@
 // header('Access-Control-Allow-Origin: *');
 // ini_set('memory_limit', '-1');
 // $con = mysqli_connect('localhost','root',null,'house_data');
-$con = mysqli_connect('localhost','reneww','renewwhouse','house_data');
-// $con = mysqli_connect('23.229.157.253','reneww','renewwhouse','house_data');
+// $con = mysqli_connect('localhost','reneww','renewwhouse','house_data');
+$con = mysqli_connect('23.229.157.253','reneww','renewwhouse','house_data'); // local testing
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
