a22ec731c7a9b927f60790e6b5772a180289b810
thomasbottonari <thomas.bottonari@gmail.com>
2015-04-29 17:51:22 -0400

    modularized the AnimatedButton using require

--- js/app/header.js
+++ js/app/header.js
@@ -1,12 +1,8 @@
-require(["../lib/raphael", "AnimatedButton", "../lib/jquery"], function() {
-	var R1 = Raphael("dataButton", 80, 30);
-	// var R2 = Raphael("tourButton", 80, 30);
-	var R3 = Raphael("aboutButton", 80, 30);
-	var R4 = Raphael("partnerButton", 90, 30);
-	var dataButton = new AnimatedButton(R1, "DATA", 80, 30);
-	// var tourButton = new AnimatedButton(R2, "TOUR", 80, 30);
-	var aboutButton = new AnimatedButton(R3, "ABOUT", 80, 30);
-	var partnerButton = new AnimatedButton(R4, "PARTNERS", 90, 30);
+require(["../lib/raphael", "AnimatedButton", "../lib/jquery"], function(Raphael, AnimatedButton, jquery) {
+	var dataButton = new AnimatedButton("dataButton", "DATA");
+	// var tourButton = new AnimatedButton("tourButton", "TOUR");
+	var aboutButton = new AnimatedButton("aboutButton", "ABOUT");
+	var partnerButton = new AnimatedButton("partnerButton", "PARTNERS");
 
 	$(window).unload(function(evt) {
         // for some reason if this function is not implemented the AnimatedButtons get stuck when using the back button in Safari
