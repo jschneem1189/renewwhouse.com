50567a4d1125206a35391710aad6833085080001
thomasbottonari <thomas.bottonari@gmail.com>
2015-05-17 17:27:42 -0400

    removed deprecated function calls

--- js/app/header.js
+++ js/app/header.js
@@ -1,14 +1,7 @@
 require(["../lib/raphael", "AnimatedButton", "../lib/jquery-2.1.4"], function(Raphael, AnimatedButton, jquery) {
-	var dataButton = new AnimatedButton("dataButton", "DATA");
-	// var tourButton = new AnimatedButton("tourButton", "TOUR");
-	var aboutButton = new AnimatedButton("aboutButton", "ABOUT");
-	var partnerButton = new AnimatedButton("partnerButton", "PARTNERS");
-
-	$(window).unload(function(evt) {
-        // for some reason if this function is not implemented the AnimatedButtons get stuck when using the back button in Safari
-        partnerButton.reset();
-        aboutButton.reset();
-        dataButton.reset();
-    });
+    var dataButton = new AnimatedButton("dataButton", "DATA");
+    // var tourButton = new AnimatedButton("tourButton", "TOUR");
+    var aboutButton = new AnimatedButton("aboutButton", "ABOUT");
+    var partnerButton = new AnimatedButton("partnerButton", "PARTNERS");
 });
 
