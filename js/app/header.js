fa12a6c85a47a90f77ea9681c3addd79d6e67ef3
thomasbottonari <thomas.bottonari@gmail.com>
2015-06-13 18:33:17 -0400

    fixed bug. screen was always blocking input.

--- js/app/header.js
+++ js/app/header.js
@@ -8,10 +8,11 @@ require(["../lib/raphael", "AnimatedButton", "../lib/jquery-2.1.4"], function(Ra
   function openMenu() {
     $('#horizontalMenu').animate({left:'-=100px'}, {duration:250});
     $('#screen').fadeTo(250, 1);
+    $('#screen').css('pointer-events','auto');
   }
   function closeMenu() {
     $('#horizontalMenu').animate({left:'100%'}, {duration:250});
-    $('#screen').fadeTo(250, 0);
+    $('#screen').fadeTo(250, 0, function() {$('#screen').css('pointer-events','none');});
   }
 
   var menuOpen = false;
