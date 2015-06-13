aefcc4d37c1f7624fe5dc4a7246589e9dde92c05
thomasbottonari <thomas.bottonari@gmail.com>
2015-06-13 18:24:11 -0400

    added gallery icon. added side menu.
    needed to add a hamburger button with side menu to fit the extra link
    to the gallery

--- js/app/header.js
+++ js/app/header.js
@@ -1,7 +1,32 @@
 require(["../lib/raphael", "AnimatedButton", "../lib/jquery-2.1.4"], function(Raphael, AnimatedButton, jquery) {
-    var dataButton = new AnimatedButton("dataButton", "DATA");
-    // var tourButton = new AnimatedButton("tourButton", "TOUR");
-    var aboutButton = new AnimatedButton("aboutButton", "ABOUT");
-    var partnerButton = new AnimatedButton("partnerButton", "PARTNERS");
+  var dataButton = new AnimatedButton("dataButton", "DATA");
+  // var tourButton = new AnimatedButton("tourButton", "TOUR");
+  var aboutButton = new AnimatedButton("aboutButton", "ABOUT");
+  var partnerButton = new AnimatedButton("partnerButton", "PARTNERS");
+  var galleryButton = new AnimatedButton("galleryButton", "GALLERY");
+
+  function openMenu() {
+    $('#horizontalMenu').animate({left:'-=100px'}, {duration:250});
+    $('#screen').fadeTo(250, 1);
+  }
+  function closeMenu() {
+    $('#horizontalMenu').animate({left:'100%'}, {duration:250});
+    $('#screen').fadeTo(250, 0);
+  }
+
+  var menuOpen = false;
+  $('#hamburgerIcon').mouseup(function() {
+    if (!menuOpen) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+    menuOpen = !menuOpen;
+  });
+
+  $('#screen').mouseup(function() {
+    closeMenu();
+    menuOpen = !menuOpen;
+  })
 });
 
