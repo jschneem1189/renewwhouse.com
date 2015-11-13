da1a4b70b045e046cc004c2903060c344c691cba
thomasbottonari <thomas.bottonari@gmail.com>
2015-11-13 17:38:38 -0500

    latest data dump had a different date format

--- data/sql scripts/import_recovery_data_from_eMonitor_csv.sql
+++ data/sql scripts/import_recovery_data_from_eMonitor_csv.sql
@@ -4,11 +4,11 @@ ALTER TABLE dailyEnergyPreNov7 ADD COLUMN `West Solar 1` decimal(14,6);
 ALTER TABLE dailyEnergyPreNov7 ADD COLUMN `West Solar 2` decimal(14,6);
 ALTER TABLE dailyEnergyPreNov7 ADD COLUMN `South Solar 1` decimal(14,6);
 ALTER TABLE dailyEnergyPreNov7 ADD COLUMN `South Solar 2` decimal(14,6);
-LOAD DATA LOCAL INFILE '/Users/thomasbottonari/Desktop/datadump.csv'
+LOAD DATA LOCAL INFILE '/Users/thomasbottonari/Desktop/totaldatadump.csv'
 INTO TABLE dailyEnergyPreNov7
 FIELDS TERMINATED BY ','
     ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (@`TS`,`Main Power Main Panel`,`West Solar 1`,`West Solar 2`,`Main Power Secondary Panel`,`South Solar 1`,`South Solar 2`)
-SET  `TS` = STR_TO_DATE(@`TS`, '%c/%e/%y');
+SET  `TS` = STR_TO_DATE(@`TS`, '%Y-%m-%d');
